9897368744873e52b80a5e052d201028b01680f5
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2024-05-07 23:43:03 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+def generate_random_data():
+        print(f"Random Number: {item}")
+
+import random
+    return data
+    for item in data:
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    data = generate_random_data()
+    main()
+def main():
